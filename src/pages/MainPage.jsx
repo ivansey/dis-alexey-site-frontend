7c62f07061f9c08cc9a628c2814021bcb3adcade
IvanSey <van.secret2015@gmail.com>
2021-10-03 22:07:59 +0300

    Update Q&A in MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -45,14 +45,28 @@ class MainPage extends Component {
                 <h3>Вопрос/Ответ</h3>
                 <ul className="collapsible">
                     <li className="active">
-                        <div className="collapsible-header">Первый вопрос</div>
-                        <div className="collapsible-body"><span>Ответ</span></div>
+                        <div className="collapsible-header">Откуда берутся паразиты?</div>
+                        <div className="collapsible-body"><p>Есть несколько вариантов</p>
+                            <p>1) Возможно они есть у соседей и попадают через щели, вытяжки, вентеляцию и т.д.</p>
+                            <p>2) Вы принесли с собой с гостиницы, работы, то есть, там где большое количество людей</p>
+                            <p>3) Есть небольшая вероятность принести с общественного транспорта или общественных мест, к примеру со школы, детского сада, больницы</p>
+                        </div>
                     </li>
                     <li>
-                        <div className="collapsible-header">Второй вопрос</div>
-                        <div className="collapsible-body"><span>Ответ</span></div>
+                        <div className="collapsible-header">После обработки их больше не будет?</div>
+                        <div className="collapsible-body"><span>Если они есть у соседей и/или работаете там, где они присутствуют, то они рано или поздно появятся</span></div>
+                    </li>
+                    <li>
+                        <div className="collapsible-header">Если заметили одного, к примеру клопа, то стоит ли бить тревогу?</div>
+                        <div className="collapsible-body"><span>Стоит! К примеру, одна самка клопа откладывает до 5 яиц в сутки. Через месяц они становятся взрослыми особями. Так же не факт, что дома только один паразит. Так что рекомендуется вызывать специалиста.</span></div>
+                    </li>
+                    <li>
+                        <div className="collapsible-header">Возможно ли самому избавиться от паразитов?</div>
+                        <div className="collapsible-body"><span>Возможно, но это может вам выйти дороже, чем вызов специалиста. Пользуйтесь услугами проверенных специалистов, к примеру мной.</span></div>
                     </li>
                 </ul>
+                <br/>
+                <p>НИКОГДА не пользуйтесь химией, свойств которой, вы не знаете!</p>
             </div>
 		</div>;
 	}
