b99fee7b2198fc6fe6105e027c7db78b5f4a0a3c
IvanSey <van.secret2015@gmail.com>
2021-10-04 16:13:40 +0300

    Add meta tags

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import Index from "./pages/Index.jsx";
 
-ReactDOM.render(<BrowserRouter><Index/></BrowserRouter>, document.querySelector("#root"));
+const rootEl = document.getElementById("root");
+if (rootEl.hasChildNodes()) {
+    ReactDOM.hydrate(<BrowserRouter><Index/></BrowserRouter>, document.querySelector("#root"));
+} else {
+    ReactDOM.render(<BrowserRouter><Index/></BrowserRouter>, document.querySelector("#root"));
+}
