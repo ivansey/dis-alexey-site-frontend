a523c4502134d4e1094381951659fd7f446bcb9b
IvanSey <van.secret2015@gmail.com>
2021-10-25 19:26:51 +0300

    Add icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,81 +1,54 @@
 import React from "react";
-import { Link, withRouter } from "react-router-dom";
-import {AppBar, Toolbar, Typography, Button, useScrollTrigger, Slide} from "@mui/material";
+import {withRouter} from "react-router-dom";
+import {AppBar, Toolbar, Typography, Button, useScrollTrigger, Slide, Divider} from "@mui/material";
+import {DashboardRounded, CallRounded, AssignmentRounded, ClassRounded, LocalOfferRounded} from '@mui/icons-material';
 import store from "../store";
 
-// class Header2 extends Component {
-// 	render2() {
-// 		return <div className="navbar-fixed red">
-// 			<nav className="nav-extended red">
-// 				<div className="nav-wrapper">
-// 					<Link to="/" className="brand-logo">Дезинсекция</Link>
-// 					<ul id="nav-mobile" className="right">
-// 						<li className="hide-on-large"><a href={`tel:${store.getState().telLink}`}><i className="material-icons">call</i></a></li>
-// 						<li className="hide-on-med-and-down"><a href={`tel:${store.getState().telLink}`}>{store.getState().tel}</a></li>
-						
-// 						{
-// 							store.getState().loginStatus === true
-// 								? <li><Link to="/admin">AP</Link></li>
-// 								: null
-// 						}
-// 					</ul>
-// 				</div>
-// 				<div className="nav-content">
-//                     <ul className="tabs tabs-transparent">
-//                         <li className="tab">
-//                             <Link to="/pricelist">Услуги</Link>
-//                         </li>
-// 						<li className="tab">
-//                             <Link to="/portfolio/home">Мои обработки</Link>
-//                         </li>
-// 						<li className="tab">
-//                             <Link to="/calc">Заказать</Link>
-//                         </li>
-//                     </ul>
-//                 </div>
-// 			</nav>
-// 		</div>
-// 	}
-// }
+function HideOnScroll(props) {
+    const {children, window} = props;
 
-function HideOnScroll (props) {
-	const {children, window} = props;
+    const trigger = useScrollTrigger({
+        target: window ? window() : undefined,
+    })
 
-	const trigger = useScrollTrigger({
-		target: window ? window() : undefined,
-	})
-
-	return <Slide appear={false} director="down" in={!trigger}>
-		{children}
-	</Slide>
+    return <Slide appear={false} director="down" in={!trigger}>
+        {children}
+    </Slide>
 }
 
-function Header (props) {
-	function hrefTo (href) {
-		props.history.push(href);
-	}
+function Header(props) {
+    function hrefTo(href) {
+        props.history.push(href);
+    }
 
-	return <React.Fragment>
-		<HideOnScroll {...props}> 
-			<AppBar position="fixed">
-				<Toolbar variant="dense">
-					<Typography variant="h6" color="inherit" component="div" sx={{flexGrow: 1}} onClick={() => hrefTo("/")}>SuperAntiKlop</Typography>
-					<Button variant="text" color="inherit" href={`tel:${store.getState().telLink}`}>CALL</Button>
-					{
-						store.getState().loginStatus === true
-							? <Button color="inherit" onClick={() => hrefTo("/admin")}>AP</Button>
-							: null
-					}
-				</Toolbar>
-				<Toolbar>
-						<Button color="inherit" onClick={() => hrefTo("/pricelist")}>Услуги</Button>
-						<Button color="inherit" onClick={() => hrefTo("/portfolio/home")}>Обработки</Button>
-						<Button color="inherit" onClick={() => hrefTo("/calc")}>Заказать</Button>
-				</Toolbar>
-			</AppBar>
-		</HideOnScroll>
-		<Toolbar/>
-	</React.Fragment>
+    return <React.Fragment>
+        <HideOnScroll {...props}>
+            <AppBar position="fixed">
+                <Toolbar variant="dense">
+                    <Typography variant="h6" color="inherit" component="div" sx={{flexGrow: 1}}
+                                onClick={() => hrefTo("/")}>SuperAntiKlop</Typography>
+                    <Button variant="text" color="inherit"
+                            href={`tel:${store.getState().telLink}`}><CallRounded/></Button>
+                    {
+                        store.getState().loginStatus === true
+                            ? <Button color="inherit" onClick={() => hrefTo("/admin")}><DashboardRounded/></Button>
+                            : null
+                    }
+                </Toolbar>
+                <Toolbar>
+                    <Button color="inherit" onClick={() => hrefTo("/pricelist")}
+                            startIcon={<LocalOfferRounded/>}>Услуги</Button>
+                    <Divider/>
+                    <Button color="inherit" onClick={() => hrefTo("/portfolio/home")}
+                            startIcon={<ClassRounded/>}>Обработки</Button>
+                    <Divider/>
+                    <Button color="inherit" onClick={() => hrefTo("/calc")}
+                            startIcon={<AssignmentRounded/>}>Заказать</Button>
+                </Toolbar>
+            </AppBar>
+        </HideOnScroll>
+        <Toolbar/>
+    </React.Fragment>
 }
 
 export default withRouter(Header);
--- src/components/NewOrderButtons.jsx
+++ src/components/NewOrderButtons.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import {withRouter} from "react-router-dom";
 import {Button, ButtonGroup} from "@mui/material";
 import store from "../store";
+import {CallRounded, AssignmentRounded} from "@mui/icons-material";
 
 function NewOrderButtons (props) {
     return <ButtonGroup variant={props.variant} color={props.color}>
-        <Button href={`tel:${store.getState().telLink}`}>Позвонить</Button>
-        <Button onClick={() => props.history.push("/calc")}>Сделать заказ онлайн</Button>
+        <Button href={`tel:${store.getState().telLink}`} startIcon={<CallRounded/>}>Позвонить</Button>
+        <Button onClick={() => props.history.push("/calc")} startIcon={<AssignmentRounded/>}>Сделать заказ онлайн</Button>
     </ButtonGroup>
 }
 
